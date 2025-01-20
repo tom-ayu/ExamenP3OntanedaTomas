b5d1ab69ed9b28d811706311ffc4b3ed2db23a3a
tom-ayu <tomas.ontaneda@udla.edu.ec>
2025-01-20 07:09:46 -0500

    Agregar archivos de proyecto.

---
+++ ExamenP3OntanedaTomas/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamenP3OntanedaTomas
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ExamenP3OntanedaTomas/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ExamenP3OntanedaTomas
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
