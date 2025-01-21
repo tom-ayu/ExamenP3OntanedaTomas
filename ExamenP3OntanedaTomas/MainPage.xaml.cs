75c26bf37eda0143a419561d699a64cdc5fcc5b1
tom-ayu <tomas.ontaneda@udla.edu.ec>
2025-01-20 21:29:54 -0500

    Implementación MVVM

--- ExamenP3OntanedaTomas/MainPage.xaml.cs
+++ ExamenP3OntanedaTomas/MainPage.xaml.cs
@@ -1,42 +1,22 @@
-﻿using ExamenP3OntanedaTomas.Models;
-using ExamenP3OntanedaTomas.Repositories;
-using System.Collections.ObjectModel;
+﻿using ExamenP3OntanedaTomas.ViewModels;
 
 namespace ExamenP3OntanedaTomas
 {
     public partial class MainPage : ContentPage
     {
-        PaisAPIRepository _paisAPIRepository;
-        PaisSQLite _paisDatabase;
-        ObservableCollection<Pais> paises = new ObservableCollection<Pais>();
-
         public MainPage()
         {
             InitializeComponent();
-            _paisAPIRepository = new PaisAPIRepository();
-            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "Paises.db3");
-            _paisDatabase = new PaisSQLite(dbPath);
+            BindingContext = new PaisViewModel();
         }
 
         private async void BuscarAPIClicked(object sender, EventArgs e)
         {
-            string prompt = TOPrompt.Text;
-            var paises = (await _paisAPIRepository.DevuelvePaisAsync(prompt)).ToList();
-            var primerPais = paises.FirstOrDefault();
-            if (primerPais != null)
+            var vm = BindingContext as PaisViewModel;
+            if (vm != null) 
             {
-                var paisDB = new PaisDB
-                {
-                    NombreComun = primerPais.Name.Common,
-                    NombreOficial = primerPais.Name.Official,
-                    Region = primerPais.Region,
-                    GoogleMaps = primerPais.Maps.GoogleMaps,
-                    OpenStreetMaps = primerPais.Maps.OpenStreetMaps
-                };
-                await _paisDatabase.GuardarPaisAsync(paisDB);
-                BindingContext = paisDB;
+                await vm.BuscarPaisAsync();
             }
         }
-
     }
 }
---
+++ ExamenP3OntanedaTomas/ViewModels/PaisViewModel.cs
@@ -0,0 +1,83 @@
+﻿using ExamenP3OntanedaTomas.Models;
+using ExamenP3OntanedaTomas.Repositories;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ExamenP3OntanedaTomas.ViewModels
+{
+    public class PaisViewModel : INotifyPropertyChanged
+    {
+        private string _prompt;
+        private PaisDB _paisSelec;
+
+        public ObservableCollection<PaisDB> Paises { get; set; }
+        public PaisAPIRepository PaisAPIRepository { get; set; }
+        public PaisSQLite paisSQLite { get; set; }
+
+        public string Prompt
+        {
+            get => _prompt;
+            set
+            {
+                _prompt = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public PaisDB paisSelec
+        {
+            get => _paisSelec;
+            set
+            {
+                _paisSelec = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public PaisViewModel()
+        {
+            PaisAPIRepository = new PaisAPIRepository();
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "Paises.db3");
+            paisSQLite = new PaisSQLite(dbPath);
+            Paises = new ObservableCollection<PaisDB>();
+        }
+
+        public async Task BuscarPaisAsync()
+        {
+            try
+            {
+                var paises = (await PaisAPIRepository.DevuelvePaisAsync(Prompt)).ToList();
+                if (paises.Any())
+                {
+                    var primerPais = paises.First();
+                    var paisDB = new PaisDB
+                    {
+                        NombreOficial = primerPais.Name.Official,
+                        Region = primerPais.Region,
+                        GoogleMaps = primerPais.Maps.GoogleMaps,
+                        OpenStreetMaps = primerPais.Maps.OpenStreetMaps
+                    };
+
+                    await paisSQLite.GuardarPaisAsync(paisDB);
+                    paisSelec = paisDB;
+                    Paises.Add(paisDB);
+                }
+                else
+                {
+                    Prompt = "No se encontraron países.";
+                }
+            }catch(Exception ex)
+            {
+                Prompt = $"Error: {ex.Message}";
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
